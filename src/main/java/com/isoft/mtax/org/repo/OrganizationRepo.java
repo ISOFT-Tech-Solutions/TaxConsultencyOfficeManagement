5738864a5e44893ddc63533288a32d721ec14ae3
AnjaniKrPathak <eranjanipathak.it@gmail.com>
2024-05-25 17:25:11 +0530

    Updated Repo

--- src/main/java/com/isoft/mtax/org/repo/OrganizationRepo.java
+++ src/main/java/com/isoft/mtax/org/repo/OrganizationRepo.java
@@ -3,5 +3,5 @@ package com.isoft.mtax.org.repo;
 import com.isoft.mtax.org.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrganizationRepo extends JpaRepository<Long, Organization> {
+public interface OrganizationRepo extends JpaRepository<Organization,Long> {
 }
