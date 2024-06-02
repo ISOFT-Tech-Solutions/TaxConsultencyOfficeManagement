eb23f821396ef3afdca0cd9c0c800e6e2cdf09de
AnjaniKrPathak <eranjanipathak.it@gmail.com>
2024-06-03 01:26:06 +0530

    Added API TDS Customer Creation and Get Customer

--- src/main/java/com/isoft/mtax/repo/CustomerRepo.java
+++ src/main/java/com/isoft/mtax/repo/CustomerRepo.java
@@ -1,6 +1,8 @@
 package com.isoft.mtax.repo;
 
 
+import com.isoft.mtax.entity.TDSCustomer;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepo {
+public interface CustomerRepo extends JpaRepository<TDSCustomer,Long> {
 }
